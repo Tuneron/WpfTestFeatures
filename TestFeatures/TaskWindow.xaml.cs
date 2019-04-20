1bc1bb7b413a0b8c63038440d6791121eb9560e7
Tuneron <tunon2010@yandex.ua>
2019-04-20 18:57:26 +0300

    v1.0

---
+++ TestFeatures/TaskWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskWindow.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public TaskWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
